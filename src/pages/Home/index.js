310a3ea3ae4bd6f3ae721592ea3f4de73bd97a7e
Pritom <pritomkarmokar99@gmail.com>
2023-03-27 22:58:58 +0600

    fix

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1 className="my-6 text-center text-3xl font-bold">Welcome to React</h1>
+      <h1 className="my-6 text-center text-3xl font-bold">Welcome</h1>
     </div>
   );
 };
