c3ae052986482e59bf903ea945944115d44b6b6a
Pritom <pritomkarmokar99@gmail.com>
2023-03-27 23:15:16 +0600

    navlink added

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
       <h1 className="my-6 text-center text-3xl font-bold">Welcome to React</h1>
+      <NavLink to="/signup">
+        Sign up
+      </NavLink>
     </div>
   );
 };
