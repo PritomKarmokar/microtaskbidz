438e297c0183e2e25b5f5c3ce177bea80af9728e
Pritom <pritomkarmokar99@gmail.com>
2023-03-29 04:20:20 +0600

    navbar added

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,13 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import "../Navbar/index.js"
+import Navbar from "../Navbar/index.js"
 import "../../css/signup.css";
 
 const Home = () => {
   return (
     <div>
+      <Navbar>Hello</Navbar>
       <h1 className="my-6 text-center text-3xl font-bold">Welcome to React</h1>
-      <NavLink to="/signup">
-        Sign up
-      </NavLink><br></br>
-      <NavLink to="/signin">
-        Sign in
-      </NavLink>
-      {/* <Navbar>Hello</Navbar> */}
+      
     </div>
   );
 };
--- src/pages/Navbar/index.js
+++ src/pages/Navbar/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "../../css/navbar.css";
 
 function Navbar(){
     return(
         <nav className="nav">
+        <a className ="site-title" href="/">Rain Design</a>
+        <ul>
+            <li><p><NavLink to="/signin">Sign In</NavLink></p></li>
             
+            <li><p><NavLink to="/signup">Sign Up</NavLink></p></li> 
+        </ul>
         </nav>
     )
 }
